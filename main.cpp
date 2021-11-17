91d39f1a1262062a1d42a651c46aac2fce274a5b
Mathis Logemann <mathisloge@gmail.com>
2021-11-17 17:36:41 +0100

    more "same" testing

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <boost/filesystem.hpp>
+#include <clocale>
 #include <iostream>
+#define NOMINMAX
+#include <windows.h>
+
+static std::wstring utf8_to_utf16(std::string const &str)
+{
+    std::wstring wstr;
+    int size = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, 0, 0);
+    if (size > 0)
+    {
+        std::vector<wchar_t> buffer(size);
+        MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &buffer[0], size);
+        wstr.assign(buffer.begin(), buffer.end() - 1);
+    }
+    return wstr;
+}
+
 int main(int argc, char const *argv[])
 {
     std::cout << "test my own output" << std::endl;
     std::system("testcreate.exe");
 
-    boost::filesystem::path p{"xxxxx"};
-    for (int i = 0; i < 10; i++)
-        p /= "xxxxx";
-    p /= "mytest.txt";
-    if (!boost::filesystem::exists(p))
+    std::string p{"test-data/shp/mytest.txt"};
+    if (!boost::filesystem::exists(utf8_to_utf16(p)))
         throw std::runtime_error("does not exists");
 
     boost::filesystem::remove(p);
--- main_create.cpp
+++ main_create.cpp
@@ -4,12 +4,8 @@
 
 int main(int argc, char const *argv[])
 {
-    boost::filesystem::path p{"xxxxx"};
-    for (int i = 0; i < 10; i++)
-        p /= "xxxxx";
-
-    boost::filesystem::create_directories(p);
-    std::ofstream file((p / "mytest.txt").string(), std::ios::trunc | std::ios::binary);
+    std::string p{"test-data/shp/mytest.txt"};
+    std::ofstream file(p, std::ios::trunc | std::ios::binary);
     file << "TEST" << std::endl;
 
     std::clog << " test output" << std::endl;
