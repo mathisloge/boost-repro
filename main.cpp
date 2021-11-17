e038e9c3e01cff8ca476d2c53916ba4d3f37cfc2
Mathis Logemann <mathisloge@gmail.com>
2021-11-17 16:41:57 +0100

    create in submodule

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@ int main(int argc, char const *argv[])
 
     std::system("testcreate.exe");
 
-    boost::filesystem::path p{"xxxxx"};
-    for (int i = 0; i < 10; i++)
-        p /= "xxxxx";
+    boost::filesystem::path p{boost::filesystem::path{"test-data"} / "shp"};
+
     if (!boost::filesystem::exists(p / "mytest.txt"))
         throw std::runtime_error("does not exists");
     boost::filesystem::remove(p / "mytest.txt");
--- main_create.cpp
+++ main_create.cpp
@@ -3,11 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-    boost::filesystem::path p{"xxxxx"};
-    for (int i = 0; i < 10; i++)
-        p /= "xxxxx";
-    boost::filesystem::create_directories(p);
-    
+    boost::filesystem::path p{boost::filesystem::path{"test-data"} / "shp"};
+
     std::ofstream file((p / "mytest.txt").string(), std::ios::trunc | std::ios::binary);
     file << "TEST" << std::endl;
     return 0;
